f1fd2ae8ec1de8b695fd01294d555dde88622afc
Negocio <julianmartinez_93@hotmail.com>
2022-05-26 11:59:09 -0300

    comenzando Extras

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var object ={};
+  for(var clave in objeto){
+    object += objeto[clave]
+  }
+  return object
 }
 
 
